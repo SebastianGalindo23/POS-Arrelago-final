dbd625cfe516a82db0c4debf0a1842f150d95a8d
sebas <sebas@EGGDESK>
2024-11-26 22:13:46 -0600

    Ya esta en ventas

--- Controllers/VentasController.cs
+++ Controllers/VentasController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using POS.Data;
 
 namespace POS.Controllers
 {
     public class VentasController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public VentasController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(string? search, string? filter)
+        {
+            var productos = _context.Productos.AsQueryable();
+
+            // Filtrar por el tipo de búsqueda
+            if (!string.IsNullOrEmpty(search) && !string.IsNullOrEmpty(filter))
+            {
+                switch (filter.ToLower())
+                {
+                    case "nombre":
+                        productos = productos.Where(p => p.Nombre.Contains(search));
+                        break;
+
+                    case "codigo":
+                        productos = productos.Where(p => p.Codigo.Contains(search));
+                        break;
+
+                    case "precio":
+                        if (decimal.TryParse(search, out var precio))
+                        {
+                            productos = productos.Where(p => p.Precio == precio);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            var listaProductos = productos.ToList();
+            return View(listaProductos);
         }
     }
 }
